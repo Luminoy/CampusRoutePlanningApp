127c503758c90c1eedeebb2f964e1fee3e3e9cb3
lumin <lab@kunshan541.com>
2016-11-02 20:07:42 +0800

    构建拓扑关系完成。

--- CampusRoutePlanningApp/Form1.cs
+++ CampusRoutePlanningApp/Form1.cs
@@ -10,6 +10,11 @@ using System.Windows.Forms;
 using ComponentFactory.Krypton.Toolkit;
 using ComponentFactory.Krypton.Ribbon;
 using DotSpatial.Symbology;
+using System.IO;
+using DotSpatial.Data;
+using GeoAPI.Geometries;
+using System.Collections;
+using static CampusRoutePlanningApp.MyUtility;
 
 namespace CampusRoutePlanningApp
 {
@@ -17,7 +22,7 @@ namespace CampusRoutePlanningApp
     {
         private FeatureLayer campusLayer;
         private KryptonRibbonGroupButton lastButton;
-        private double[,] roadmaps;
+        private PointLineRelation PointLineTopology = new PointLineRelation();
         public Form1()
         {
             InitializeComponent();
@@ -38,8 +43,87 @@ namespace CampusRoutePlanningApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            campusLayer = map1.AddLayer("GeoData//Campus_Union.shp") as FeatureLayer;
+            campusLayer = map1.AddLayer(@"GeoData\roadmaps.shp") as FeatureLayer;
             map1.FunctionMode = DotSpatial.Controls.FunctionMode.Pan;
+
+            // 构建拓扑关系
+            TopologyConstruction(campusLayer.DataSet);
+        }
+
+        public class PointEqualityComparer: IEqualityComparer<PointF>
+        {
+            public bool Equals(PointF P1, PointF P2)
+            {
+                return ((P1.X - P2.X) < 1e-6) && ((P1.Y - P2.Y) < 1e-6);
+            }
+
+            public int GetHashCode(PointF obj)
+            {
+                if (obj == null)
+                    return 0;
+                else
+                    return obj.X.ToString().GetHashCode() + obj.Y.ToString().GetHashCode();
+            }
+        }
+        private void TopologyConstruction(IFeatureSet pFeatureSet)
+        {
+            List<TopoPoint> pTopoPoints = new List<TopoPoint>();
+            List<PointF> XYs = new List<PointF>();
+            List<Line> LINEs = new List<Line>();
+            //HashSet<TopoPoint> m_HashSet = new HashSet<TopoPoint>();
+            int nLineCount = 0;
+            
+            foreach (IFeature pFeature in pFeatureSet.Features)
+            {
+                ILineString pLine = pFeature.Geometry as ILineString;
+                int nPointCount = pLine.NumPoints;
+                for (int k = 0; k < nPointCount - 1; k++)
+                {
+                    IPoint P1 = pLine.GetPointN(k);
+                    IPoint P2 = pLine.GetPointN(k + 1);
+
+                    PointF pf1 = new PointF();
+                    pf1.X = (float)P1.X;
+                    pf1.Y = (float)P1.Y;
+                    XYs.Add(pf1);
+
+                    PointF pf2 = new PointF();
+                    pf2.X = (float)P2.X;
+                    pf2.Y = (float)P2.Y;
+                    XYs.Add(pf2);
+
+                    Line line = new Line();
+                    line.StartPoint = pf1;
+                    line.EndPoint = pf2;
+                    line.LineNumber = nLineCount++;
+                    LINEs.Add(line);
+                }
+            }
+            List<PointF> newXYs = XYs.Distinct(new PointEqualityComparer()).ToList();
+
+            IDictionary<PointF, int> hashTable = new Dictionary<PointF, int>(new PointEqualityComparer());
+            for (int k = 0; k < newXYs.Count; k++)
+            {
+                hashTable.Add(newXYs[k], k);
+                pTopoPoints.Add(new TopoPoint());
+            }
+            
+            for(int k = 0; k < LINEs.Count; k++)
+            {
+                int nId1 = hashTable[LINEs[k].StartPoint];
+                int nId2 = hashTable[LINEs[k].EndPoint];
+                pTopoPoints[nId1].nLineCount++;
+                pTopoPoints[nId1].pConnectedLineIDs.Add(k);
+                pTopoPoints[nId2].nLineCount++;
+                pTopoPoints[nId2].pConnectedLineIDs.Add(k);
+            }
+
+            PointLineTopology.PointCount = newXYs.Count;
+            PointLineTopology.LineCount = LINEs.Count;
+            PointLineTopology.pPoints = newXYs;
+            PointLineTopology.pLines = LINEs;
+            PointLineTopology.pTopoPoints = pTopoPoints;
+
         }
 
         private void kryptonRibbonGroupButton1_Click(object sender, EventArgs e)
@@ -74,6 +158,7 @@ namespace CampusRoutePlanningApp
         private void kryptonRibbonGroupButton6_Click(object sender, EventArgs e)
         {
             // 获取起点坐标
+            
         }
 
         private void kryptonRibbonGroupButton7_Click(object sender, EventArgs e)
---
+++ CampusRoutePlanningApp/MyUtility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusRoutePlanningApp
+{
+    public class MyUtility
+    {
+        public class TopoPoint
+        {
+            public int nLineCount { get; set; }
+            public List<long> pConnectedLineIDs = new List<long>();
+
+        }
+
+        public class Line
+        {
+            public int LineNumber { get; set; }
+            public PointF StartPoint { get; set; }
+            
+            public PointF EndPoint { get; set; }
+
+        }
+        public class PointLineRelation
+        {
+            public int PointCount { get; set; }
+            public int LineCount { get; set; }
+            public List<TopoPoint> pTopoPoints{ get; set; }
+            public List<PointF> pPoints { get; set; }
+            public List<Line> pLines { get; set; }
+            public TopoPoint GetPointByIndex(int K)
+            {
+                if ((K >= PointCount) || (pTopoPoints == null)) return null;
+                return pTopoPoints[K];
+            }
+
+        }
+    }
+    
+}
